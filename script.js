dbdc75e85ee9a578197f5f75a4a73545d0520d5b
FOTOPECHATY <boogdansokolov7@gmail.com>
2025-05-01 18:01:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('uploadForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Фотография загружена!');
+});
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Сообщение отправлено!');
+});
